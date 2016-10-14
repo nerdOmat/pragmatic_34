96096bfeef496a9c4d3ff6f6e0bb620d5eedb805
nerdOmat <ghandi_84@hotmail.com>
2016-10-14 16:23:37 +0200

    improved path_variables in theme.lua

--- themes/ticktronaut/theme.lua
+++ themes/ticktronaut/theme.lua
@@ -5,8 +5,8 @@
 theme = {}
 -- change paths to your needs ---------------------------
 config_path = (os.getenv("HOME") .. "/.config/awesome/")
---themes_path = config_path .. "themes/"
---mytheme = "ticktronaut"
+themes_path = config_path .. "themes/"
+mytheme = "ticktronaut"
 -- change paths to your needs ---------------------------
 
 theme.font          = "sans 8"
@@ -37,15 +37,15 @@ theme.border_marked = "#91231c"
 --theme.taglist_bg_focus = "#ff0000"
 
 -- Display the taglist squares
-theme.taglist_squares_sel   = config_path .. "themes/ticktronaut/taglist/squarefw.png"
-theme.taglist_squares_unsel = config_path .. "themes/ticktronaut/taglist/squarew.png"
+theme.taglist_squares_sel   = themes_path .. mytheme .. "/" .. taglist/squarefw.png"
+theme.taglist_squares_unsel = themes_path .. mytheme .. "/" .. taglist/squarew.png"
 
-theme.tasklist_floating_icon = config_path .. "themes/ticktronaut/tasklist/floatingw.png"
+theme.tasklist_floating_icon = themes_path .. mytheme .. "/" .. tasklist/floatingw.png"
 
 -- Variables set for theming the menu:
 -- menu_[bg|fg]_[normal|focus]
 -- menu_[border_color|border_width]
-theme.menu_submenu_icon = config_path .. "themes/ticktronaut/submenu.png"
+theme.menu_submenu_icon = themes_path .. mytheme .. "/" .. submenu.png"
 theme.menu_height = "15"
 theme.menu_width  = "100"
 
@@ -55,29 +55,29 @@ theme.menu_width  = "100"
 --theme.bg_widget = "#cc0000"
 
 -- Define the image to load
---theme.titlebar_close_button_normal = config_path .. "themes/ticktronaut/titlebar/close_normal.png"
-theme.titlebar_close_button_normal = config_path .. "themes/ticktronaut/titlebar/close_normal.png"
-theme.titlebar_close_button_focus  = config_path .. "themes/ticktronaut/titlebar/close_focus.png"
+--theme.titlebar_close_button_normal = themes_path .. mytheme .. "/" .. titlebar/close_normal.png"
+theme.titlebar_close_button_normal = themes_path .. mytheme .. "/"titlebar/close_normal.png"
+theme.titlebar_close_button_focus  = themes_path .. mytheme .. "/" .. titlebar/close_focus.png"
 
-theme.titlebar_ontop_button_normal_inactive = config_path .. "themes/ticktronaut/titlebar/ontop_normal_inactive.png"
-theme.titlebar_ontop_button_focus_inactive  = config_path .. "themes/ticktronaut/titlebar/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_active = config_path .. "themes/ticktronaut/titlebar/ontop_normal_active.png"
-theme.titlebar_ontop_button_focus_active  = config_path .. "themes/ticktronaut/titlebar/ontop_focus_active.png"
+theme.titlebar_ontop_button_normal_inactive = themes_path .. mytheme .. "/" .. titlebar/ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_inactive  = themes_path .. mytheme .. "/" .. titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_active = themes_path .. mytheme .. "/" .. titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_active  = themes_path .. mytheme .. "/" .. titlebar/ontop_focus_active.png"
 
-theme.titlebar_sticky_button_normal_inactive = config_path .. "themes/ticktronaut/titlebar/sticky_normal_inactive.png"
-theme.titlebar_sticky_button_focus_inactive  = config_path .. "themes/ticktronaut/titlebar/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_active = config_path .. "themes/ticktronaut/titlebar/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_active  = config_path .. "themes/ticktronaut/titlebar/sticky_focus_active.png"
+theme.titlebar_sticky_button_normal_inactive = themes_path .. mytheme .. "/" .. titlebar/sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_inactive  = themes_path .. mytheme .. "/" .. titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_active = themes_path .. mytheme .. "/" .. titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_active  = themes_path .. mytheme .. "/" .. titlebar/sticky_focus_active.png"
 
-theme.titlebar_floating_button_normal_inactive = config_path .. "themes/ticktronaut/titlebar/floating_normal_inactive.png"
-theme.titlebar_floating_button_focus_inactive  = config_path .. "themes/ticktronaut/titlebar/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_active = config_path .. "themes/ticktronaut/titlebar/floating_normal_active.png"
-theme.titlebar_floating_button_focus_active  = config_path .. "themes/ticktronaut/titlebar/floating_focus_active.png"
+theme.titlebar_floating_button_normal_inactive = themes_path .. mytheme .. "/" .. titlebar/floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_inactive  = themes_path .. mytheme .. "/" .. titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_active = themes_path .. mytheme .. "/" .. titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_active  = themes_path .. mytheme .. "/" .. titlebar/floating_focus_active.png"
 
-theme.titlebar_maximized_button_normal_inactive = config_path .. "themes/ticktronaut/titlebar/maximized_normal_inactive.png"
-theme.titlebar_maximized_button_focus_inactive  = config_path .. "themes/ticktronaut/titlebar/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_normal_active = config_path .. "themes/ticktronaut/titlebar/maximized_normal_active.png"
-theme.titlebar_maximized_button_focus_active  = config_path .. "themes/ticktronaut/titlebar/maximized_focus_active.png"
+theme.titlebar_maximized_button_normal_inactive = themes_path .. mytheme .. "/" .. titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_inactive  = themes_path .. mytheme .. "/" .. titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_active = themes_path .. mytheme .. "/" .. titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_active  = themes_path .. mytheme .. "/" .. titlebar/maximized_focus_active.png"
 
 -- You can use your own command to set your wallpaper
 --theme.wallpaper_cmd = { "awsetbg /home/usappz/.config/awesome/themes/ticktronaut/background.png" }
@@ -86,18 +86,18 @@ theme.wallpaper_cmd = { "awsetbg /home/usappz/.config/awesome/themes/ticktronaut
 --theme.wallpaper_cmd = { "awsetbg /home/usappz/.config/awesome/themes/ticktronaut/wallpapers/powerarrow.jpg" }
 
 -- You can use your own layout icons like this:
-theme.layout_fairh = config_path .. "themes/ticktronaut/layouts/fairhw.png"
-theme.layout_fairv = config_path .. "themes/ticktronaut/layouts/fairvw.png"
-theme.layout_floating  = config_path .. "themes/ticktronaut/layouts/floatingw.png"
-theme.layout_magnifier = config_path .. "themes/ticktronaut/layouts/magnifierw.png"
-theme.layout_max = config_path .. "themes/ticktronaut/layouts/maxw.png"
-theme.layout_fullscreen = config_path .. "themes/ticktronaut/layouts/fullscreenw.png"
-theme.layout_tilebottom = config_path .. "themes/ticktronaut/layouts/tilebottomw.png"
-theme.layout_tileleft   = config_path .. "themes/ticktronaut/layouts/tileleftw.png"
-theme.layout_tile = config_path .. "themes/ticktronaut/layouts/tilew.png"
-theme.layout_tiletop = config_path .. "themes/ticktronaut/layouts/tiletopw.png"
-theme.layout_spiral  = config_path .. "themes/ticktronaut/layouts/spiralw.png"
-theme.layout_dwindle = config_path .. "themes/ticktronaut/layouts/dwindlew.png"
+theme.layout_fairh = themes_path .. mytheme .. "/" .. layouts/fairhw.png"
+theme.layout_fairv = themes_path .. mytheme .. "/" .. layouts/fairvw.png"
+theme.layout_floating  = themes_path .. mytheme .. "/" .. layouts/floatingw.png"
+theme.layout_magnifier = themes_path .. mytheme .. "/" .. layouts/magnifierw.png"
+theme.layout_max = themes_path .. mytheme .. "/" .. layouts/maxw.png"
+theme.layout_fullscreen = themes_path .. mytheme .. "/" .. layouts/fullscreenw.png"
+theme.layout_tilebottom = themes_path .. mytheme .. "/" .. layouts/tilebottomw.png"
+theme.layout_tileleft   = themes_path .. mytheme .. "/" .. layouts/tileleftw.png"
+theme.layout_tile = themes_path .. mytheme .. "/" .. layouts/tilew.png"
+theme.layout_tiletop = themes_path .. mytheme .. "/" .. layouts/tiletopw.png"
+theme.layout_spiral  = themes_path .. mytheme .. "/" .. layouts/spiralw.png"
+theme.layout_dwindle = themes_path .. mytheme .. "/" .. layouts/dwindlew.png"
 
 theme.awesome_icon = "/usr/share/awesome/icons/awesome16.png"
 
